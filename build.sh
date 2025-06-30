545bb00ed90c7bee28d0abb98764237db295c6da
khalid-ans <163677702+khalid-ans@users.noreply.github.com>
2025-06-30 21:53:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Install system dependencies
 apt-get update && apt-get install -y tesseract-ocr
+pip install tesseract
+pip install tesseract-ocr
 # Install Python dependencies
 pip install -r requirements.txt 
